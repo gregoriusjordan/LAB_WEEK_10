e14619ea39b4c606e847f43d9c2687210496285e
Gregorius Jordan <gregoriusjordan@gmail.com>
2025-11-24 00:40:03 +0700

    Commit No. 2: Implement LiveData to Sync Activity and Fragment UI

--- app/src/main/java/com/example/lab_week_10/FirstFragment.kt
+++ app/src/main/java/com/example/lab_week_10/FirstFragment.kt
@@ -5,55 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.R
+import com.example.lab_week_10.viewmodels.TotalViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FirstFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        prepareViewModel()
+    }
+    private fun updateText(total: Int) {
+        view?.findViewById<TextView>(R.id.text_total)?.text =
+            getString(R.string.text_total, total)
+    }
+    private fun prepareViewModel() {
+        val viewModel =
+            ViewModelProvider(requireActivity()).get(TotalViewModel::class.java)
+// Observe the LiveData object
+        viewModel.total.observe(viewLifecycleOwner, {
+// Whenever the value of the LiveData object changes
+// the updateText() is called, with the new value as the parameter
+            updateText(it)
+        })
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FirstFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            FirstFragment()
     }
+
 }
--- app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
+++ app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
@@ -2,15 +2,23 @@ package com.example.lab_week_10.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+
 
 class TotalViewModel: ViewModel() {
-    val total = MutableLiveData<Int>()
-
+    //Declare the LiveData object
+    private val _total = MutableLiveData<Int>()
+    val total: LiveData<Int> = _total
+    //Initialize the LiveData object
     init {
-        total.value = 0
+        //postValue is used to set the value of the LiveData object
+        //from a background thread or the main thread
+        //While on the other hand setValue() is used
+        //only if you're on the main thread
+        _total.postValue(0)
     }
-
+    //Increment the total value
     fun incrementTotal() {
-        total.value = (total.value ?: 0) + 1
+        _total.postValue(_total.value?.plus(1))
     }
 }
